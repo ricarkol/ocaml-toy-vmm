80cd114eb27db563ae82f976d2c45c9597ca4e55
Ricardo Koller <ricarkol@gmail.com>
2020-08-16 23:57:10 +0000

    ocamlformat

--- bindings.ml
+++ bindings.ml
@@ -1,213 +1,229 @@
-module Stubs =
-functor
-  (S : Cstubs_structs.TYPE)
-  ->
-  struct
-    module Kvm_userspace_memory_region = struct
-      type kvm_userspace_memory_region
+module Stubs (S : Cstubs_structs.TYPE) = struct
+  module Kvm_userspace_memory_region = struct
+    type kvm_userspace_memory_region
 
-      type t = kvm_userspace_memory_region Ctypes.structure
+    type t = kvm_userspace_memory_region Ctypes.structure
 
-      let t : t S.typ = S.structure "kvm_userspace_memory_region"
+    let t : t S.typ = S.structure "kvm_userspace_memory_region"
 
-      let slot = S.(field t "slot" int32_t)
+    let slot = S.(field t "slot" int32_t)
 
-      let flags = S.(field t "flags" int32_t)
+    let flags = S.(field t "flags" int32_t)
 
-      let guest_phys_addr = S.(field t "guest_phys_addr" int64_t)
+    let guest_phys_addr = S.(field t "guest_phys_addr" int64_t)
 
-      let memory_size = S.(field t "memory_size" int64_t)
+    let memory_size = S.(field t "memory_size" int64_t)
 
-      let userspace_addr = S.(field t "userspace_addr" (ptr char))
+    let userspace_addr = S.(field t "userspace_addr" (ptr char))
 
-      let () = S.seal t
-    end
+    let () = S.seal t
+  end
 
-    module Kvm_run = struct
-      type kvm_run
+  module Kvm_run = struct
+    type kvm_run
 
-      type t = kvm_run Ctypes.structure
+    type t = kvm_run Ctypes.structure
 
-      let t : t S.typ = S.structure "kvm_run"
+    let t : t S.typ = S.structure "kvm_run"
 
-      let exit_reason = S.(field t "exit_reason" int)
+    let exit_reason = S.(field t "exit_reason" int)
 
-      let io_port = S.(field t "io.port" int16_t)
+    let io_port = S.(field t "io.port" int16_t)
 
-      let io_data_offset = S.(field t "io.data_offset" int64_t)
+    let io_data_offset = S.(field t "io.data_offset" int64_t)
 
-      let () = S.seal t
-    end
+    let () = S.seal t
+  end
 
-    module Kvm_regs = struct
-      type kvm_regs
+  module Kvm_regs = struct
+    type kvm_regs
 
-      type t = kvm_regs Ctypes.structure
+    type t = kvm_regs Ctypes.structure
 
-      let t : t S.typ = S.structure "kvm_regs"
+    let t : t S.typ = S.structure "kvm_regs"
 
-      let rflags = S.(field t "rflags" int64_t)
+    let rflags = S.(field t "rflags" int64_t)
 
-      let rip = S.(field t "rip" int64_t)
-      
-      let rsi = S.(field t "rsi" int64_t)
+    let rip = S.(field t "rip" int64_t)
 
-      let () = S.seal t
-    end
+    let rsi = S.(field t "rsi" int64_t)
 
-    module Kvm_sregs = struct
-      type kvm_sregs
+    let () = S.seal t
+  end
 
-      type t = kvm_sregs Ctypes.structure
+  module Kvm_sregs = struct
+    type kvm_sregs
 
-      let t : t S.typ = S.structure "kvm_sregs"
+    type t = kvm_sregs Ctypes.structure
 
-      let cs_selector = S.(field t "cs.selector" int16_t)
+    let t : t S.typ = S.structure "kvm_sregs"
 
-      let cs_base = S.(field t "cs.base" int64_t)
-      let cs_limit = S.(field t "cs.limit" int32_t)
-      let cs_g = S.(field t "cs.g" int8_t)
-      let cs_db = S.(field t "cs.db" int8_t)
+    let cs_selector = S.(field t "cs.selector" int16_t)
 
-      let ss_selector = S.(field t "ss.selector" int16_t)
+    let cs_base = S.(field t "cs.base" int64_t)
 
-      let ss_base = S.(field t "ss.base" int64_t)
-      let ss_limit = S.(field t "ss.limit" int32_t)
-      let ss_g = S.(field t "ss.g" int8_t)
-      let ss_db = S.(field t "ss.db" int8_t)
+    let cs_limit = S.(field t "cs.limit" int32_t)
 
-      let ds_selector = S.(field t "ds.selector" int16_t)
+    let cs_g = S.(field t "cs.g" int8_t)
 
-      let ds_base = S.(field t "ds.base" int64_t)
-      let ds_limit = S.(field t "ds.limit" int32_t)
-      let ds_g = S.(field t "ds.g" int8_t)
+    let cs_db = S.(field t "cs.db" int8_t)
 
-      let es_selector = S.(field t "es.selector" int16_t)
+    let ss_selector = S.(field t "ss.selector" int16_t)
 
-      let es_base = S.(field t "es.base" int64_t)
-      let es_limit = S.(field t "es.limit" int32_t)
-      let es_g = S.(field t "es.g" int8_t)
+    let ss_base = S.(field t "ss.base" int64_t)
 
-      let fs_selector = S.(field t "fs.selector" int16_t)
+    let ss_limit = S.(field t "ss.limit" int32_t)
 
-      let fs_base = S.(field t "fs.base" int64_t)
-      let fs_limit = S.(field t "fs.limit" int32_t)
-      let fs_g = S.(field t "fs.g" int8_t)
+    let ss_g = S.(field t "ss.g" int8_t)
 
-      let gs_selector = S.(field t "gs.selector" int16_t)
+    let ss_db = S.(field t "ss.db" int8_t)
 
-      let gs_base = S.(field t "gs.base" int64_t)
-      let gs_limit = S.(field t "gs.limit" int32_t)
-      let gs_g = S.(field t "gs.g" int8_t)
-      
-      let cr0 = S.(field t "cr0" int64_t)
+    let ds_selector = S.(field t "ds.selector" int16_t)
 
-      let () = S.seal t
-    end
+    let ds_base = S.(field t "ds.base" int64_t)
 
-    module Kvm_cpuid_entry2 = struct
-      type kvm_cpuid_entry2
+    let ds_limit = S.(field t "ds.limit" int32_t)
 
-      type t = kvm_cpuid_entry2 Ctypes.structure
+    let ds_g = S.(field t "ds.g" int8_t)
 
-      let t : t S.typ = S.structure "kvm_cpuid_entry2"
+    let es_selector = S.(field t "es.selector" int16_t)
 
-      let _function = S.(field t "function" uint32_t)
-      let eax = S.(field t "eax" uint32_t)
-      let ebx = S.(field t "ebx" uint32_t)
-      let ecx = S.(field t "ecx" uint32_t)
-      let edx = S.(field t "edx" uint32_t)
+    let es_base = S.(field t "es.base" int64_t)
 
-      let () = S.seal t
-    end
+    let es_limit = S.(field t "es.limit" int32_t)
 
-    module Kvm_cpuid2 = struct
-      type kvm_cpuid2
+    let es_g = S.(field t "es.g" int8_t)
 
-      type t = kvm_cpuid2 Ctypes.structure
+    let fs_selector = S.(field t "fs.selector" int16_t)
 
-      let t : t S.typ = S.structure "kvm_cpuid2"
+    let fs_base = S.(field t "fs.base" int64_t)
 
-      let nent = S.(field t "nent" uint32_t)
-      let padding = S.(field t "padding" uint32_t)
-      (*let entries = S.(field t "entries" (ptr void))*)
+    let fs_limit = S.(field t "fs.limit" int32_t)
 
-      let () = S.seal t
-    end
+    let fs_g = S.(field t "fs.g" int8_t)
 
-    module Kvm_pit_config = struct
-      type kvm_pit_config
+    let gs_selector = S.(field t "gs.selector" int16_t)
 
-      type t = kvm_pit_config Ctypes.structure
+    let gs_base = S.(field t "gs.base" int64_t)
 
-      let t : t S.typ = S.structure "kvm_pit_config"
+    let gs_limit = S.(field t "gs.limit" int32_t)
 
-      let flags = S.(field t "flags" int32_t)
+    let gs_g = S.(field t "gs.g" int8_t)
 
-      let () = S.seal t
-    end
+    let cr0 = S.(field t "cr0" int64_t)
 
-    module Boot_params = struct
-      type boot_params
+    let () = S.seal t
+  end
 
-      type t = boot_params Ctypes.structure
+  module Kvm_cpuid_entry2 = struct
+    type kvm_cpuid_entry2
 
-      let t : t S.typ = S.structure "boot_params"
+    type t = kvm_cpuid_entry2 Ctypes.structure
 
-      let () = S.seal t
-    end
+    let t : t S.typ = S.structure "kvm_cpuid_entry2"
 
+    let _function = S.(field t "function" uint32_t)
 
-    module Limits = struct
-      let shrt_max = S.(constant "SHRT_MAX" short)
-    end
+    let eax = S.(field t "eax" uint32_t)
 
-    module Kvm = struct
-      let _KVM_CREATE_VM = S.(constant "KVM_CREATE_VM" int)
+    let ebx = S.(field t "ebx" uint32_t)
 
-      let _KVM_SET_USER_MEMORY_REGION =
-        S.(constant "KVM_SET_USER_MEMORY_REGION" int)
+    let ecx = S.(field t "ecx" uint32_t)
 
-      let _KVM_CREATE_VCPU = S.(constant "KVM_CREATE_VCPU" int)
+    let edx = S.(field t "edx" uint32_t)
 
-      let _KVM_GET_VCPU_MMAP_SIZE = S.(constant "KVM_GET_VCPU_MMAP_SIZE" int)
+    let () = S.seal t
+  end
 
-      let _KVM_GET_SREGS = S.(constant "KVM_GET_SREGS" int)
+  module Kvm_cpuid2 = struct
+    type kvm_cpuid2
 
-      let _KVM_SET_SREGS = S.(constant "KVM_SET_SREGS" int)
+    type t = kvm_cpuid2 Ctypes.structure
 
-      let _KVM_GET_REGS = S.(constant "KVM_GET_REGS" int)
+    let t : t S.typ = S.structure "kvm_cpuid2"
 
-      let _KVM_SET_REGS = S.(constant "KVM_SET_REGS" int)
+    let nent = S.(field t "nent" uint32_t)
 
-      let _KVM_RUN = S.(constant "KVM_RUN" int)
+    let padding = S.(field t "padding" uint32_t)
 
-      let _KVM_GET_SUPPORTED_CPUID = S.(constant "KVM_GET_SUPPORTED_CPUID" int)
+    (*let entries = S.(field t "entries" (ptr void))*)
 
-      let _KVM_CPUID_SIGNATURE = S.(constant "KVM_CPUID_SIGNATURE" int)
+    let () = S.seal t
+  end
 
-      let _KVM_CPUID_FEATURES = S.(constant "KVM_CPUID_FEATURES" int)
+  module Kvm_pit_config = struct
+    type kvm_pit_config
 
-      let _KVM_SET_CPUID2 = S.(constant "KVM_SET_CPUID2" int)
+    type t = kvm_pit_config Ctypes.structure
 
-      let _KVM_SET_TSS_ADDR = S.(constant "KVM_SET_TSS_ADDR" int)
+    let t : t S.typ = S.structure "kvm_pit_config"
 
-      let _KVM_SET_IDENTITY_MAP_ADDR = S.(constant "KVM_SET_IDENTITY_MAP_ADDR" int)
+    let flags = S.(field t "flags" int32_t)
 
-      let _KVM_CREATE_IRQCHIP = S.(constant "KVM_CREATE_IRQCHIP" int)
+    let () = S.seal t
+  end
 
-      let _KVM_CREATE_PIT = S.(constant "KVM_CREATE_PIT" int)
-      
-      let _KVM_CREATE_PIT2 = S.(constant "KVM_CREATE_PIT2" int)
-    end
+  module Boot_params = struct
+    type boot_params
 
-    module Kvm_exit = struct
-      let _KVM_EXIT_IO = S.(constant "KVM_EXIT_IO" int)
+    type t = boot_params Ctypes.structure
 
-      let _KVM_EXIT_SHUTDOWN = S.(constant "KVM_EXIT_SHUTDOWN" int)
-    end
+    let t : t S.typ = S.structure "boot_params"
 
-    (*
+    let () = S.seal t
+  end
+
+  module Limits = struct
+    let shrt_max = S.(constant "SHRT_MAX" short)
+  end
+
+  module Kvm = struct
+    let _KVM_CREATE_VM = S.(constant "KVM_CREATE_VM" int)
+
+    let _KVM_SET_USER_MEMORY_REGION =
+      S.(constant "KVM_SET_USER_MEMORY_REGION" int)
+
+    let _KVM_CREATE_VCPU = S.(constant "KVM_CREATE_VCPU" int)
+
+    let _KVM_GET_VCPU_MMAP_SIZE = S.(constant "KVM_GET_VCPU_MMAP_SIZE" int)
+
+    let _KVM_GET_SREGS = S.(constant "KVM_GET_SREGS" int)
+
+    let _KVM_SET_SREGS = S.(constant "KVM_SET_SREGS" int)
+
+    let _KVM_GET_REGS = S.(constant "KVM_GET_REGS" int)
+
+    let _KVM_SET_REGS = S.(constant "KVM_SET_REGS" int)
+
+    let _KVM_RUN = S.(constant "KVM_RUN" int)
+
+    let _KVM_GET_SUPPORTED_CPUID = S.(constant "KVM_GET_SUPPORTED_CPUID" int)
+
+    let _KVM_CPUID_SIGNATURE = S.(constant "KVM_CPUID_SIGNATURE" int)
+
+    let _KVM_CPUID_FEATURES = S.(constant "KVM_CPUID_FEATURES" int)
+
+    let _KVM_SET_CPUID2 = S.(constant "KVM_SET_CPUID2" int)
+
+    let _KVM_SET_TSS_ADDR = S.(constant "KVM_SET_TSS_ADDR" int)
+
+    let _KVM_SET_IDENTITY_MAP_ADDR =
+      S.(constant "KVM_SET_IDENTITY_MAP_ADDR" int)
+
+    let _KVM_CREATE_IRQCHIP = S.(constant "KVM_CREATE_IRQCHIP" int)
+
+    let _KVM_CREATE_PIT = S.(constant "KVM_CREATE_PIT" int)
+
+    let _KVM_CREATE_PIT2 = S.(constant "KVM_CREATE_PIT2" int)
+  end
+
+  module Kvm_exit = struct
+    let _KVM_EXIT_IO = S.(constant "KVM_EXIT_IO" int)
+
+    let _KVM_EXIT_SHUTDOWN = S.(constant "KVM_EXIT_SHUTDOWN" int)
+  end
+
+  (*
 #define KVM_EXIT_UNKNOWN          0
 #define KVM_EXIT_EXCEPTION        1
 #define KVM_EXIT_IO               2
@@ -230,4 +246,4 @@ functor
 #define KVM_EXIT_PAPR_HCALL	  19
 #define KVM_EXIT_S390_UCONTROL	  20
 *)
-  end
+end
--- main.ml
+++ main.ml
@@ -29,108 +29,99 @@ let init_regs vcpu_fd =
   let _ = ioctl vcpu_fd _KVM_GET_SREGS (to_voidp (addr sregs)) in
   (* XXX: check for failure *)
   let () =
-    setf sregs Kvm_sregs.cs_base Int64.zero;
-    setf sregs Kvm_sregs.cs_limit Int32.max_int;
-    setf sregs Kvm_sregs.cs_g 1;
-
-    setf sregs Kvm_sregs.ds_base Int64.zero;
-    setf sregs Kvm_sregs.ds_limit Int32.max_int;
-    setf sregs Kvm_sregs.ds_g 1;
-
-    setf sregs Kvm_sregs.es_base Int64.zero;
-    setf sregs Kvm_sregs.es_limit Int32.max_int;
-    setf sregs Kvm_sregs.es_g 1;
-
-    setf sregs Kvm_sregs.fs_base Int64.zero;
-    setf sregs Kvm_sregs.fs_limit Int32.max_int;
-    setf sregs Kvm_sregs.fs_g 1;
-    
-    setf sregs Kvm_sregs.gs_base Int64.zero;
-    setf sregs Kvm_sregs.gs_limit Int32.max_int;
-    setf sregs Kvm_sregs.gs_g 1;
-   
-    setf sregs Kvm_sregs.ss_base Int64.zero;
-    setf sregs Kvm_sregs.ss_limit Int32.max_int;
-    setf sregs Kvm_sregs.ss_g 1;
-
-    setf sregs Kvm_sregs.cs_db 1;
-    setf sregs Kvm_sregs.ss_db 1;
-    setf sregs Kvm_sregs.cr0 (Int64.logor (getf sregs Kvm_sregs.cr0) Int64.one);
+    setf sregs Kvm_sregs.cs_base Int64.zero ;
+    setf sregs Kvm_sregs.cs_limit Int32.max_int ;
+    setf sregs Kvm_sregs.cs_g 1 ;
+    setf sregs Kvm_sregs.ds_base Int64.zero ;
+    setf sregs Kvm_sregs.ds_limit Int32.max_int ;
+    setf sregs Kvm_sregs.ds_g 1 ;
+    setf sregs Kvm_sregs.es_base Int64.zero ;
+    setf sregs Kvm_sregs.es_limit Int32.max_int ;
+    setf sregs Kvm_sregs.es_g 1 ;
+    setf sregs Kvm_sregs.fs_base Int64.zero ;
+    setf sregs Kvm_sregs.fs_limit Int32.max_int ;
+    setf sregs Kvm_sregs.fs_g 1 ;
+    setf sregs Kvm_sregs.gs_base Int64.zero ;
+    setf sregs Kvm_sregs.gs_limit Int32.max_int ;
+    setf sregs Kvm_sregs.gs_g 1 ;
+    setf sregs Kvm_sregs.ss_base Int64.zero ;
+    setf sregs Kvm_sregs.ss_limit Int32.max_int ;
+    setf sregs Kvm_sregs.ss_g 1 ;
+    setf sregs Kvm_sregs.cs_db 1 ;
+    setf sregs Kvm_sregs.ss_db 1 ;
+    setf sregs Kvm_sregs.cr0 (Int64.logor (getf sregs Kvm_sregs.cr0) Int64.one)
   in
   let regs = Ctypes.make Stubs.Kvm_regs.t in
   let () =
-    Ctypes.setf regs Kvm_regs.rflags (Int64.of_int 2);
-    Ctypes.setf regs Kvm_regs.rip (Int64.of_int 0x100000);
-    Ctypes.setf regs Kvm_regs.rsi (Int64.of_int 0x10000);
+    Ctypes.setf regs Kvm_regs.rflags (Int64.of_int 2) ;
+    Ctypes.setf regs Kvm_regs.rip (Int64.of_int 0x100000) ;
+    Ctypes.setf regs Kvm_regs.rsi (Int64.of_int 0x10000)
   in
   (sregs, regs)
 
 let get_region size file_path =
   let region = Ctypes.make Stubs.Kvm_userspace_memory_region.t in
   (*let mem_fd = Unix.openfile "/dev/shm/test"*)
-  let mem_fd = Unix.openfile file_path [ Unix.O_RDWR ] 0o656 in
+  let mem_fd = Unix.openfile file_path [Unix.O_RDWR] 0o656 in
   (*[Unix.O_RDWR; Unix.O_TRUNC; Unix.O_CREAT] 0o656 in*)
   let buf =
-    array1_of_genarray (Unix.map_file mem_fd char c_layout false [| size |])
+    array1_of_genarray (Unix.map_file mem_fd char c_layout false [|size|])
   in
   (*let a = array_of_bigarray array1 buf in*)
   let () =
     let open Ctypes in
-    setf region slot Int32.zero;
-    setf region flags Int32.zero;
-    setf region guest_phys_addr Int64.zero;
-    setf region memory_size (Int64.of_int size);
+    setf region slot Int32.zero ;
+    setf region flags Int32.zero ;
+    setf region guest_phys_addr Int64.zero ;
+    setf region memory_size (Int64.of_int size) ;
     setf region userspace_addr (bigarray_start array1 buf)
   in
   (region, mem_fd)
 
 let main () =
   let open Ctypes in
-  let kvm_fd = openfile "/dev/kvm" [ O_RDWR ] 0 in
+  let kvm_fd = openfile "/dev/kvm" [O_RDWR] 0 in
   let () = Printf.printf "kvm_fd = %d\n" (int_of_file_descr kvm_fd) in
   let fd = Int32.of_int (int_of_file_descr kvm_fd) in
   let vm_fd = ioctl (int_of_file_descr kvm_fd) _KVM_CREATE_VM null in
-
-  let _ = ioctl vm_fd _KVM_SET_TSS_ADDR (ptr_of_raw_address (Nativeint.of_int 0xffffd000)) in
-  
+  let _ =
+    ioctl vm_fd _KVM_SET_TSS_ADDR
+      (ptr_of_raw_address (Nativeint.of_int 0xffffd000))
+  in
   (*
   let map_addr = 0xffffc000 in
   let map_addr_voidp = coerce (ptr void) (ptr int) (addr map_addr) in
   let _ = ioctl vm_fd _KVM_SET_IDENTITY_MAP_ADDR map_addr_voidp in
 *)
-
-
   let _ = ioctl vm_fd _KVM_CREATE_IRQCHIP null in
-
   let pit = Ctypes.make Kvm_pit_config.t in
   let () = Ctypes.setf pit Kvm_pit_config.flags Int32.zero in
   let _ = ioctl vm_fd _KVM_CREATE_PIT Ctypes.(to_voidp (addr pit)) in
-
   let () = Printf.printf "vm_fd = %d\n" vm_fd in
   let size = 1_073_741_824 in
-
   let region, mem_fd = get_region size "bzImage" in
   let _ =
     ioctl vm_fd _KVM_SET_USER_MEMORY_REGION Ctypes.(to_voidp (addr region))
   in
-
   let vcpu_fd = ioctl vm_fd _KVM_CREATE_VCPU null in
   let () = Printf.printf "vcpu_fd = %d\n" vcpu_fd in
-
-  let (sregs, regs) = init_regs vcpu_fd in
+  let sregs, regs = init_regs vcpu_fd in
   let _ = ioctl vcpu_fd _KVM_SET_SREGS Ctypes.(to_voidp (addr sregs)) in
   let _ = ioctl vcpu_fd _KVM_SET_REGS Ctypes.(to_voidp (addr regs)) in
-
   let kvm_cpuid = make Kvm_cpuid2.t in
   let () = setf kvm_cpuid Kvm_cpuid2.nent Unsigned.UInt32.zero in
-  let _ = ioctl (int_of_file_descr kvm_fd) _KVM_GET_SUPPORTED_CPUID Ctypes.(to_voidp (addr kvm_cpuid)) in
-  let _ = ioctl (int_of_file_descr kvm_fd) _KVM_SET_CPUID2 Ctypes.(to_voidp (addr kvm_cpuid)) in
-
+  let _ =
+    ioctl (int_of_file_descr kvm_fd) _KVM_GET_SUPPORTED_CPUID
+      Ctypes.(to_voidp (addr kvm_cpuid))
+  in
+  let _ =
+    ioctl (int_of_file_descr kvm_fd) _KVM_SET_CPUID2
+      Ctypes.(to_voidp (addr kvm_cpuid))
+  in
   let kvm_run_mmap_size =
     ioctl (int_of_file_descr kvm_fd) _KVM_GET_VCPU_MMAP_SIZE null
   in
   let () = Printf.printf "vcpu_mmap_size = %d\n" kvm_run_mmap_size in
-
   (* PROT_READ | PROT_WRITE is 3 *)
   (* MAP_SHARED is 1 *)
   let run_addr =
@@ -142,14 +133,11 @@ let main () =
     | None -> raise (MmapExc "mmap returned null")
     | Some p -> p
   in
-
   let run_ptr = coerce (ptr void) (ptr Stubs.Kvm_run.t) run_addr in
-
   let run = !@run_ptr in
   let () =
     Printf.printf "exit_reason = %d\n" (getf run Stubs.Kvm_run.exit_reason)
   in
-
   let rec process_exit reason =
     match reason with
     | z when z == _KVM_EXIT_IO ->
@@ -170,44 +158,11 @@ let main () =
     | z when z == _KVM_EXIT_SHUTDOWN -> Printf.printf "shutdown\n"
     | x -> Printf.printf "unknown exit %d\n" x
   in
-
   let ret = ioctl vcpu_fd _KVM_RUN null in
   let () = Printf.printf "kvm_exit_io = %d\n" Stubs.Kvm_exit._KVM_EXIT_IO in
   let () = process_exit (getf run Stubs.Kvm_run.exit_reason) in
-
   let () = close mem_fd in
   let () = close (file_descr_of_int vm_fd) in
   close kvm_fd
 
 let () = main ()
-
-(*
-NOTES:
-
-Cstruct.of_bigarray
-https://github.com/avsm/mirage-duniverse/blob/983e115ff5a9fb37e3176c373e227e9379f0d777/ocaml_modules/cstruct/ppx_test/basic.ml#L131
-
-http://lists.ocaml.org/pipermail/ctypes/2016-February/000196.html
-
-ctypes:
-http://ocamllabs.io/ocaml-ctypes/Ctypes.html
-bigarray_of_ptr
-
-https://dev.realworldocaml.org/runtime-memory-layout.html
-
-https://github.com/mirage/mmap/blob/master/test/test.ml
-
-    let a = array1_of_genarray (Mmap.V1.map_file fd float64 c_layout true [|10000|])    in
-    for i = 0 to 9999 do a.{i} <- float i done;
-
-http://lists.ocaml.org/pipermail/ctypes/2013-December/000017.html
-
-struct in struct
-https://github.com/ocamllabs/ocaml-ctypes/blob/master/examples/fts/foreign/fts.ml
-
-let p_addr = Ctypes.allocate (ptr mystruct) (from_voidp mystruct null) in
-let err = myfunc p_addr in
-let p = !@ p_addr in
-https://stackoverflow.com/questions/49166644/ocaml-ctypes-how-to-make-the-pointer-to-pointer
-
-*)
